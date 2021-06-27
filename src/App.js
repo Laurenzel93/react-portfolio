1ffe6f35dbd78c69f05efa950176ad3108b33872
Lauren Wenzel <Laurenzel93@aol.com>
2021-06-27 15:55:05 -0400

    added images

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Helmet } from 'react-helmet';
 import Navbar from './components/Navbar';
@@ -19,8 +18,10 @@ function App() {
         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
         <link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet"></link>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"></link>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
         <title>You found me!</title>
+
       </Helmet>
 
       <Navbar />
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import './style.css';
 
 function Home() {
     return (
-        <div ClassName="hero-image">
+        <div ClassName="hero-image" style={{ backgroundImage: "url(https://wallpaperaccess.com/full/2502689.jpg)" }}>
             <div ClassName="hero-text">
                 <p>Hi, I'm Lauren Wenzel</p>
                 <h2>And I like to make things on the internet!</h2>
